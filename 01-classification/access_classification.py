dc188dbfb75096cd9f54b53d65ad988e3ee70b36
André Formento <andreformento.sc@gmail.com>
2019-03-24 17:43:44 -0300

    taxa de erro

--- 01-classification/access_classification.py
+++ 01-classification/access_classification.py
@@ -1,18 +1,27 @@
 from dados import carregar_acessos
 X, Y = carregar_acessos()
 
+treino_dados = X[:90]
+treino_marcacoes = Y[:90]
+
+teste_dados = X[-9:]
+teste_marcacoes = Y[-9:]
+
 #print(Y)
 #print(X)
 
 from sklearn.naive_bayes import MultinomialNB
 modelo = MultinomialNB()
-modelo.fit(X, Y)
+# treinar
+modelo.fit(treino_dados, treino_marcacoes)
 
-resultado = modelo.predict(X)
-diferencas = resultado - Y
+# prevendo
+resultado = modelo.predict(teste_dados)
+
+diferencas = resultado - teste_marcacoes
 acertos = [d for d in diferencas if d == 0]
 total_de_acertos = len(acertos)
-total_de_elementos = len(X)
+total_de_elementos = len(teste_dados)
 taxa_de_acertos = 100.0 * (total_de_acertos / total_de_elementos)
 
 print(taxa_de_acertos)
