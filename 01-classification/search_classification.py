89e9d721633cb6b5d9d8fdb12cb9cb3fd4d17d47
André Formento <andreformento.sc@gmail.com>
2019-03-24 19:51:29 -0300

    yes no

--- 01-classification/search_classification.py
+++ 01-classification/search_classification.py
@@ -1,23 +1,13 @@
+from collections import Counter
 import pandas as pd
+
 df = pd.read_csv('busca.csv')
 
-X_df = df[['home', 'busca', 'logado', 'comprou']]
+X_df = df[['home', 'busca', 'logado']]
 Y_df = df['comprou']
 
 Xdummies = pd.get_dummies(X_df)
 Ydummies = Y_df
-
-# a eficacia do algoritmo que chuta 0 ou 1
-#acerto_de_um = len(Y_df[Y_df=='yes'])
-#acerto_de_zero = len(Y_df[Y_df=='no'])
-#acerto_de_um = list(Y_df).count('yes')
-#acerto_de_zero = list(Y_df).count('no')
-#maior_acerto = max(acerto_de_um, acerto_de_zero)
-
-from collections import Counter
-maior_acerto = max(Counter(Y_df).values())
-taxa_de_acerto_base = (100.0 * maior_acerto) / len(Y_df)
-print(f"Taxa de acerto base {taxa_de_acerto_base}")
 
 # print("elementos do Y do qual no índice do X é zero:")
 # print(X_df[Y_df==0])
@@ -26,6 +16,7 @@ X = Xdummies.values
 Y = Ydummies.values
 
 porcentagem_de_treino = 0.9
+
 tamanho_de_treino = int(porcentagem_de_treino * len(Y))
 tamanho_de_teste = len(Y) - tamanho_de_treino
 
@@ -36,6 +27,7 @@ teste_dados = X[-tamanho_de_teste:]
 teste_marcacoes = Y[-tamanho_de_teste:]
 
 from sklearn.naive_bayes import MultinomialNB
+
 modelo = MultinomialNB()
 # treinar
 modelo.fit(treino_dados, treino_marcacoes)
@@ -46,6 +38,11 @@ resultado = modelo.predict(teste_dados)
 acertos = (resultado == teste_marcacoes)
 total_de_acertos = sum(acertos)
 total_de_elementos = len(teste_dados)
-taxa_de_acertos = 100.0 * (total_de_acertos / total_de_elementos)
+taxa_de_acerto = 100.0 * total_de_acertos / total_de_elementos
 
-print(f"Taxa de acertos = {taxa_de_acertos}")
+print(f"Taxa de acerto = {taxa_de_acerto}")
+
+# a eficacia do algoritmo que chuta tudo o mesmo valor
+maior_acerto = max(Counter(teste_marcacoes).values())
+taxa_de_acerto_base = 100.0 * maior_acerto / len(teste_marcacoes)
+print(f"Taxa de acerto base {taxa_de_acerto_base}")
