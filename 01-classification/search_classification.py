4dcaeb64e60ed1179f9790b761499b59af65afe6
André Formento <andreformento.sc@gmail.com>
2019-03-24 19:29:58 -0300

    yes no

--- 01-classification/search_classification.py
+++ 01-classification/search_classification.py
@@ -8,13 +8,13 @@ Xdummies = pd.get_dummies(X_df)
 Ydummies = Y_df
 
 # a eficacia do algoritmo que chuta 0 ou 1
-acerto_de_um = len(Y_df[Y_df==1])
-acerto_de_zero = len(Y_df[Y_df==0])
+acerto_de_um = len(Y_df[Y_df=='yes'])
+acerto_de_zero = len(Y_df[Y_df=='no'])
 taxa_de_acerto_base = (100.0 * max(acerto_de_um, acerto_de_zero)) / len(Y_df)
 print(f"Taxa de acerto base {taxa_de_acerto_base}")
 
-print("elementos do Y do qual no índice do X é zero:")
-print(X_df[Y_df==0])
+# print("elementos do Y do qual no índice do X é zero:")
+# print(X_df[Y_df==0])
 
 X = Xdummies.values
 Y = Ydummies.values
@@ -37,9 +37,8 @@ modelo.fit(treino_dados, treino_marcacoes)
 # prevendo
 resultado = modelo.predict(teste_dados)
 
-diferencas = resultado - teste_marcacoes
-acertos = [d for d in diferencas if d == 0]
-total_de_acertos = len(acertos)
+acertos = (resultado == teste_marcacoes)
+total_de_acertos = sum(acertos)
 total_de_elementos = len(teste_dados)
 taxa_de_acertos = 100.0 * (total_de_acertos / total_de_elementos)
 
